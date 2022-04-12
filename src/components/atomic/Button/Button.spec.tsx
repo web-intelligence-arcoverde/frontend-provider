ef1265578cdfde6893ea732a68c72b958ca8e52a
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-12 07:56:10 -0300

    Base pronto para iniciar Tasks

--- src/components/atomic/Button/Button.spec.tsx
+++ src/components/atomic/Button/Button.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import renderer from 'react-test-renderer'
-import { Button } from 'src/components/Button/Button'
+import { Button } from 'src/components/atomic/Button/Button'
 
 describe('Button Component', () => {
   it('should render correcty', () => {
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,7 @@
 import { lazy, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Home from 'src/pages/Home'
 
-const Example = lazy(() =>
-  import('src/pages/Example/Example').then(module => ({
-    default: module.Example,
-  })),
-)
 const GenericNotFound = lazy(() =>
   import('src/pages/GenericNotFound/GenericNotFound').then(module => ({
     default: module.GenericNotFound,
@@ -16,7 +12,7 @@ export const Routes = () => {
   return (
     <Suspense fallback={<p>Carregando...</p>}>
       <Switch>
-        <Route exact path="/" component={Example} />
+        <Route exact path="/" component={Home} />
 
         <Route path="/404" component={GenericNotFound} />
         <Redirect to="/404" />
