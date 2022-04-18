079a1b84b219759d2ee104ba119c43a06610e033
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-18 09:20:38 -0300

    adicionando keyframes ao slide

--- src/components/molecules/Slide/index.tsx
+++ src/components/molecules/Slide/index.tsx
@@ -13,17 +13,23 @@ const items = [
   {
     src: 'https://livedemo00.template-help.com/wt_57837/images/page-1_slide01.jpg?1650025268578%27',
     altText: 'Slide 1',
-    caption: 'Slide 1',
+    caption: 'Maneiras inovadoras de facilitar a comunicação',
+    describe:
+      'Avaliamos as tradições de trabalho árduo e acreditamos que esta é a pedra angular de qualquer empresa de sucesso.',
   },
   {
     src: 'https://livedemo00.template-help.com/wt_57837/images/page-1_slide02.jpg?1650025269155%27',
     altText: 'Slide 2',
-    caption: 'Slide 2',
+    caption: 'Conexões de alta velocidade entre corporações',
+    describe:
+      'Avaliamos as tradições de trabalho árduo e acreditamos que esta é a pedra angular de qualquer empresa de sucesso.',
   },
   {
     src: 'https://livedemo00.template-help.com/wt_57837/images/page-1_slide03.jpg?1650025320653%27',
     altText: 'Slide 3',
-    caption: 'Slide 3',
+    caption: 'Conjunto progressivo de conectividade com a Internet',
+    describe:
+      'Avaliamos as tradições de trabalho árduo e acreditamos que esta é a pedra angular de qualquer empresa de sucesso.',
   },
 ]
 
@@ -55,9 +61,9 @@ const Slide = ({ props }: any) => {
         onExited={() => setAnimating(false)}
         key={item.src}
       >
-        <img className="teste" src={item.src} alt={item.altText} />
+        <img src={item.src} alt={item.altText} />
         <CarouselCaption
-          captionText={item.caption}
+          captionText={item.describe}
           captionHeader={item.caption}
         />
       </CarouselItem>
--- src/components/molecules/Slide/style.ts
+++ src/components/molecules/Slide/style.ts
@@ -1,21 +1,32 @@
-import styled from 'styled-components';
+import styled,{keyframes} from 'styled-components';
 import { COLORS } from 'src/common';
 
+const FadeInAnimation = keyframes`  
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
+
 export const Container = styled.div`
-  
+ 
   .carousel-item{
       height: 100vh;
-      
+      animation: ${FadeInAnimation} 1s ease-in-out;
+     
   }
 .carousel-item h3{
       position: absolute;
       bottom: 350px;
-      left: 45%;
+      left: 20%;
+      font-size: 30px;
+    
   }
   .carousel-item p{ 
       position: absolute;
       bottom: 250px;
-      left: 46%;
+      width: 70%;
+      left: 20%;
+      font-size: 18px;
+      
       
   }
   .carousel-indicators{
