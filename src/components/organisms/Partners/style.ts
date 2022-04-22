d1f14729e22c950e4888e82e356b4b82e6f7d972
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-22 11:47:40 -0300

    paterns ajustado

--- src/components/organisms/Partners/style.ts
+++ src/components/organisms/Partners/style.ts
@@ -13,6 +13,12 @@ export const Container = styled.div`
   h1 {
     font-size: 54px;
     line-height: 50px;
+    @media (max-width:769px){
+      font-size: 33px;
+      margin-top: 40px;
+     line-height: 30px;
+    }
+    
   }
   p {
     width: 80%;
@@ -24,26 +30,38 @@ export const Container = styled.div`
     height: auto;
     padding: 20px 0;
     p {
+      width: 100%;
       padding: 0 10%;
+      font-size:16px ;
     }
   }
 `
 export const DividerContainer = styled.div`
   border-bottom: 4px solid ${COLORS.white};
-  width: 2%;
+  width: 5%;
   margin: 10px 0 80px 0;
+  @media (max-width:769px) {
+    width: 15%;
+    margin: 0;
+    margin-bottom: 50px;
+  }
 `
 export const PartnersLogoContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  gap: 60px;
+  gap: 80px;
   img {
     width: 100px;
     height: 100px;
     :hover {
       opacity: 0.5;
+    }
+    @media (max-width:769px){
+      width: 75px;
+      height: 75px;
+
     }
   }
   margin-bottom: 55px;
