a6d6dfc6781b32b1cbc51c1d6f9f682b1a242568
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-25 11:10:42 -0300

    teste mobile

--- src/__mocks__/nav.tsx
+++ src/__mocks__/nav.tsx
@@ -2,5 +2,5 @@ export const NavMock = [
   { name: 'Home', href: '#home', offset: 0 },
   { name: 'Sobre', href: '#about', offset: -30 },
   { name: 'Serviços', href: '#services', offset: 100 },
-  { name: 'Parceiros', href: '#partners', offset: 0 },
+  { name: 'Parceiros', href: '#partners', offset: 30 },
 ]
